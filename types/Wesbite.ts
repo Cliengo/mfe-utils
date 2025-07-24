5beca1f68f8815dcd6f0f02382da1a68f6822dad
Daniel Gomez <daniel.gomez@cliengo.com>
2025-07-24 15:45:34 -0300

    update website with new fields

--- types/Wesbite.ts
+++ types/Wesbite.ts
@@ -21,4 +21,8 @@ export interface Website {
   isWhatsAppChat: boolean;
   externalId?: string;
   whatsappNumber? : string;
+  metaAccountName?: string;
+  whatsAppApiStatus: 'CONNECTED' | 'PENDING_CONNECTION' | 'PENDING_DISCONNECTION' | 'NOT_CONNECTED';
+  isWhatsAppApi?: boolean;
+  isWhatsAppLite?: boolean;
 }
